70466ee4769cb3d02c4c7c8ea156c31c8905d3e8
Developer <Developer@DEV-M01>
2022-08-12 15:13:31 +0100

    Add project files.

---
+++ Models/Weather.cs
@@ -0,0 +1,11 @@
+﻿namespace HttpClientAPIDemo.Models
+{
+    public class Weather
+    {
+        public int Id { get; set; }
+        public string Main { get; set; }
+        public string Description { get; set; }
+    }
+
+
+}
---
+++ Models/WeatherForecastModel.cs
@@ -0,0 +1,17 @@
+﻿namespace HttpClientAPIDemo.Models
+{
+
+    public class WeatherForecastModel
+    {
+        public Weather[] Coord { get; set; }
+        public Weather[] Weather { get; set; }
+        public string Base { get; set; }
+        public int Visibility { get; set; }
+        public int Dt { get; set; }
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int Cod { get; set; }
+    }
+
+
+}
